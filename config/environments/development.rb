a05581e7a8d94805d09b28eb2a5f5533b1924cbc
Alex <aplatteeuw@outlook.fr>
2021-03-12 10:31:14 +0000

    add config for cloud9 server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "ffa3146d48524ff9951f9c6959256900.vfs.cloud9.us-east-2.amazonaws.com"
 end
