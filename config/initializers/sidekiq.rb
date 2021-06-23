a50e3cbb8045bb04aa324b68d1ccf295e8f755f9
Alexandre Platteeuw <aplatteeuw@outlook.fr>
2021-06-23 16:20:24 +0200

    comment schedule.yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('HEROKU_REDIS_TEAL_URL') { "redis://localhost:6379/1" }, size: 12, network_timeout: 5 }
-  schedule_file = "config/schedule.yml"
+  # schedule_file = "config/schedule.yml"
 
   # if File.exist?(schedule_file) && Sidekiq.server?
   #   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
